a091b29ac714117cb7be2a1bf94d82d303c7f695
You-Wen Luo <luorich115970@gmail.com>
2022-05-25 16:12:30 +0800

    Add random seeds of campgrounds in database, show them in the home page, and add links to see details in show page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/my-yelp-camp', {
     useNewUrlParser: true,
@@ -21,6 +22,17 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/:id', async (req, res) =>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground } )
+
 })
 
 app.listen(3000, () => {
