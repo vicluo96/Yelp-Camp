2113154d465e94f4721410478d2b826b5da30bdc
You-Wen Luo <luorich115970@gmail.com>
2022-06-24 19:45:46 +0800

    Add image upload in new.ejs and edit.ejs forms: allow to add and delete multiple imgs of campgrounds

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    //Image transformations of cloudinary
+    return this.url.replace('/upload', '/upload/w_200')
+})
+
 const CampgroundSchema = new Schema({
     title: String,
-    image: String,
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
